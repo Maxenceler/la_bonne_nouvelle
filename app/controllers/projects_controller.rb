b522188a57855b20a036564e1e5440c0e120f541
Maxence Lerouge <maxencelerouge1@gmail.com>
2021-08-26 11:33:21 +0200

    index finished unless some CSS

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,4 +12,18 @@ class ProjectsController < ApplicationController
 
   def new
   end
+
+  def create
+    @project = Project.new(params_project)
+    @project.user = current_user
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :index
+    end
+  end
+
+  def params_project
+    params.require(:project).permit(:address, :project_type, :estimated_price, :garden_size, :living_area, :rooms_number, :building_date, :non_living_area, :bathrooms_number, :bedrooms_number, :title, :buildable)
+  end
 end
