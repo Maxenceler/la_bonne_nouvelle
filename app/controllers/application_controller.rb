dba4a39352a011fe23e9b5f066af3e61f9e2576a
violette ruccio <violetteruccio@gmail.com>
2021-08-27 11:56:15 +0200

    liens ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    projects_path
+  end
+
+  def after_sign_up_path_for(resource)
+    projects_path
+  end
+
 end
