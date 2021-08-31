b194c5e006dc3b8d2b8e95c3c66517e3e50ddf9b
estelle dourthe <estelle.dourthe40@orange.fr>
2021-08-31 15:13:21 +0200

    geocoder a completer

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :response_projects, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_28_154858) do
+ActiveRecord::Schema.define(version: 2021_08_31_122623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 2021_08_28_154858) do
     t.string "buildable"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "longitude"
+    t.float "latitude"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
