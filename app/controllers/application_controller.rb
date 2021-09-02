e23a6fa2749051d547830f12868a5d43ab006869
violette ruccio <violetteruccio@gmail.com>
2021-09-02 12:04:07 +0200

    check list ok avc fav icon et nom de domaine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
